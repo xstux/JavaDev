bf32fec6ab1e9e406ac7219bdb4121f7e69bcce9
Stuart <xstux@hotmail.com>
2022-06-06 10:27:56 +0100

    Update

---
+++ Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Stuart");
+
+        int myFirstNumber = 69;
+
+        System.out.println("My first number is " + myFirstNumber);
+        
+    }
+
+
+}
