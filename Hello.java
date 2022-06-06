a5d9b164b0eae93110c502122e93d907f49a96ea
Stuart <xstux@hotmail.com>
2022-06-06 15:23:00 +0100

    Test

--- Hello.java
+++ Hello.java
@@ -1,14 +1,2 @@
-public class Hello {
+package stuart.learn1;
 
-    public static void main(String[] args) {
-
-        System.out.println("Hello Stuart");
-
-        int myFirstNumber = 69;
-
-        System.out.println("My first number is " + myFirstNumber);
-        
-    }
-
-
-}
---
+++ stuart/learn1/Hello.java
@@ -0,0 +1,19 @@
+package stuart.learn1;
+
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Stuart");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int myLastOne = 1000 - myTotal;
+
+        System.out.println("My last one is " + myLastOne);
+    }
+
+}
